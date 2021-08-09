5ec8a11f2b34eaf78b76722c5fae8990ac563536
hyy1987 <779003812@qq.com>
2021-08-09 15:19:15 +0800

    fix(compiler): fix some bug

--- src/utils/fixMenuItemIcon.ts
+++ src/utils/fixMenuItemIcon.ts
@@ -1,19 +1,20 @@
-// import React from 'react';
+import React from 'react';
 import type { MenuDataItem } from '@ant-design/pro-layout';
-// import * as allIcons from '@ant-design/icons';
+import * as allIcons from '@ant-design/icons';
 
-// FIX从接口获取菜单时icon为string类型
-// const fixMenuItemIcon = (menus: MenuDataItem[], iconType='Outlined'): MenuDataItem[] => {
-const fixMenuItemIcon = (menus: MenuDataItem[]): MenuDataItem[] => {
-  // menus.forEach((item) => {
-  //   const {icon, children} = item;
-  //   if (typeof icon === 'string') {
-  //     const fixIconName = icon.slice(0,1).toLocaleUpperCase()+icon.slice(1) + iconType;
-  //     item.icon = React.createElement(allIcons[fixIconName] || allIcons[icon]);
-  //   }
-  //   children && children.length>0 ? item.children = fixMenuItemIcon(children) : null;
-  // });
-  return menus;
+const fixMenuItemIcon = (menus: MenuDataItem[], iconType = 'Outlined'): MenuDataItem[] => {
+  return menus.map((item) => {
+    let fixIcon: React.ReactNode;
+    if (typeof item.icon === 'string') {
+      const { icon } = item;
+      const fixIconName = icon.slice(0, 1).toLocaleUpperCase() + icon.slice(1) + iconType;
+      fixIcon = React.createElement(allIcons[fixIconName] || allIcons[icon]);
+    }
+    if (item.children && item.children.length > 0) {
+      return { ...item, icon: fixIcon, children: fixMenuItemIcon(item.children) };
+    }
+    return { ...item, icon: fixIcon };
+  });
 };
 
 export default fixMenuItemIcon;
