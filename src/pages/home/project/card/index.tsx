c9ada8f8fa2c5d5ca408f02fe0f6b57070402eff
hyy1987 <779003812@qq.com>
2021-11-14 22:53:22 +0800

    feat: project card

--- src/pages/home/project/card/index.tsx
+++ src/pages/home/project/card/index.tsx
@@ -7,11 +7,16 @@ import { useRef } from 'react';
 // import { useRequest } from 'umi';
 import { getProjectList, starProject, deleteProject } from '@/services/project/api';
 import type { ProjectListItem } from '@/services/project/data';
-import styles from './style.less';
 import ProList from '@ant-design/pro-list';
 import type { ActionType } from '@ant-design/pro-table';
-import { FolderOpenOutlined, DeleteOutlined, StarFilled, StarOutlined } from '@ant-design/icons';
-import { Popconfirm } from 'antd';
+import {
+  FolderOpenOutlined,
+  DeleteOutlined,
+  StarFilled,
+  StarOutlined,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons';
+import { Button, message, Modal, Tooltip } from 'antd';
 import moment from 'moment';
 
 type ProjectListProps = {
@@ -29,111 +34,126 @@ const ListSearchApplications: FC<ProjectListProps> = (porps) => {
     starProject(id);
     actionRef.current?.reload();
   };
+  function onDelete(pkid: number) {
+    Modal.confirm({
+      title: 'Do you Want to delete this plan?',
+      icon: <ExclamationCircleOutlined />,
+      content: '',
+      onOk() {
+        deleteProject(pkid).then(async (result) => {
+          if (result === 'ok') {
+            message.success('Delete successfully!');
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          } else {
+            message.error(result);
+          }
+        });
+      },
+      onCancel() {},
+    });
+  }
   if (oldSearchValue !== searchValue) {
     oldSearchValue = searchValue;
     actionRef.current?.reload();
   }
   return (
-    <div className={styles.filterCardList}>
-      <ProList<ProjectListItem>
-        actionRef={actionRef}
-        request={(params) => {
-          return getProjectList(params, { lastUpdateTime: 'descend' }, searchValue);
-        }}
-        grid={{
-          gutter: 16,
-          xs: 1,
-          sm: 2,
-          md: 3,
-          lg: 3,
-          xl: 4,
-          xxl: 4,
-        }}
-        metas={{
-          title: {
-            dataIndex: 'name',
-            title: 'Name',
+    // <div className={styles.filterCardList}>
+    <ProList<ProjectListItem>
+      actionRef={actionRef}
+      request={(params) => {
+        return getProjectList(params, { lastUpdateTime: 'descend' }, searchValue);
+      }}
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 3,
+        xl: 4,
+        xxl: 4,
+      }}
+      metas={{
+        subTitle: {
+          dataIndex: 'name',
+        },
+        actions: {
+          render: (_, row) => {
+            return [
+              <Tooltip title={row.star ? 'No star' : 'Starred'}>
+                <Button
+                  shape="circle"
+                  icon={row.star ? <StarOutlined /> : <StarFilled />}
+                  size="small"
+                  onClick={() => {
+                    setstar(row.id);
+                  }}
+                />
+              </Tooltip>,
+              <Tooltip title="Open">
+                <Button
+                  href={`/project/plan/list?projectid=${row.id}`}
+                  target="_blank"
+                  shape="circle"
+                  icon={<FolderOpenOutlined />}
+                  size="small"
+                />
+              </Tooltip>,
+              <Tooltip title="Delete">
+                <Button
+                  shape="circle"
+                  icon={<DeleteOutlined />}
+                  size="small"
+                  onClick={() => onDelete(row.id)}
+                />
+              </Tooltip>,
+            ];
           },
-          content: {
-            render: (_, row) => {
-              return (
-                <div>
-                  <span
-                    onClick={() => {
-                      setstar(row.id);
-                    }}
-                    hidden={row.star}
-                  >
-                    <StarOutlined />
-                  </span>
-                  <span
-                    onClick={() => {
-                      setstar(row.id);
-                    }}
-                    hidden={!row.star}
-                  >
-                    <StarFilled />
-                  </span>
-                  <p>
-                    Created At: {moment(row.createTime).format('YYYY-MM-DD HH:mm:ss')}
-                    <br />
-                    Last Update: {moment(row.lastUpdateTime).format('YYYY-MM-DD HH:mm:ss')}
-                    <br />
-                    Comment: {row.comment}
-                    <br />
-                    {/* <a href={`/project/model?projectid=${row.id}`} target='_blank'> */}
-                    <a href={`/project/plan/list?projectid=${row.id}`}>
-                      <FolderOpenOutlined /> Open
-                    </a>
-                    <Popconfirm
-                      title="Are you sure to delete this task?"
-                      onConfirm={() => {
-                        deleteProject(row.id);
-                        actionRef.current?.reload();
-                      }}
-                      onCancel={() => {}}
-                      okText="Yes"
-                      cancelText="No"
-                    >
-                      <a href="#">
-                        <DeleteOutlined /> Delete
-                      </a>
-                    </Popconfirm>
-                  </p>
-                </div>
-              );
-            },
+        },
+        content: {
+          render: (_, row) => {
+            return (
+              <>
+                Created At: {moment(row.createTime).format('YYYY-MM-DD HH:mm:ss')}
+                <br />
+                Last Update: {moment(row.lastUpdateTime).format('YYYY-MM-DD HH:mm:ss')}
+                <br />
+                Comment: {row.comment}
+              </>
+            );
           },
-        }}
+        },
+      }}
 
-        // renderItem={(item) => (
-        //   <List.Item key={item.id}>
-        // <Card
-        //   hoverable
-        //   actions={[
-        //     <Tooltip key="open" title="Open">
-        //       <a href="/project/information" target="_blank">
-        //         <FolderOpenOutlined />
-        //       </a>
-        //     </Tooltip>,
-        //     <Tooltip key="delete" title="Delete">
-        //       <DeleteOutlined onClick={() => alert('delete')} />
-        //     </Tooltip>,
-        //   ]}
-        // >
-        //   <Card.Meta avatar={<Avatar size="small" src={''} />} title={item.name} />
-        //   <div className={styles.cardItemContent}>
-        //     <CardInfo
-        //       createdAt={item.createdAt}
-        //       lastUpdate={item.lastUpdate}
-        //       comment={item.comment}
-        //     />
-        //   </div>
-        // </Card>
-        // </List.Item>
-        // )}
-      />
-    </div>
+      // renderItem={(item) => (
+      //   <List.Item key={item.id}>
+      // <Card
+      //   hoverable
+      //   actions={[
+      //     <Tooltip key='open' title='Open'>
+      //       <a href='/project/information' target='_blank'>
+      //         <FolderOpenOutlined />
+      //       </a>
+      //     </Tooltip>,
+      //     <Tooltip key='delete' title='Delete'>
+      //       <DeleteOutlined onClick={() => alert('delete')} />
+      //     </Tooltip>,
+      //   ]}
+      // >
+      //   <Card.Meta avatar={<Avatar size='small' src={''} />} title={item.name} />
+      //   <div className={styles.cardItemContent}>
+      //     <CardInfo
+      //       createdAt={item.createdAt}
+      //       lastUpdate={item.lastUpdate}
+      //       comment={item.comment}
+      //     />
+      //   </div>
+      // </Card>
+      // </List.Item>
+      // )}
+    />
+    // </div>
   );
 };
 
