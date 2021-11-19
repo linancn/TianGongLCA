fd6ee7186f7e8587f284c3913947bd53c11296c6
nanli <linanenv@gmail.com>
2021-11-19 17:54:36 +0800

    fix: change default language to en-US

--- config/config.ts
+++ config/config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   },
   // https://umijs.org/zh-CN/plugins/plugin-locale
   locale: {
-    // default zh-CN
-    default: 'zh-CN',
+    // default en-US
+    default: 'en-US',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
