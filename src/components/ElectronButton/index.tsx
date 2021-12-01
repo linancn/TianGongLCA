90f47eb9adaa47c0f3f070665f899389ade296f0
hyy1987 <779003812@qq.com>
2021-12-01 17:21:33 +0800

    fix: ElectronButton

--- src/components/ElectronButton/index.tsx
+++ src/components/ElectronButton/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from 'antd';
-import { ExpandAltOutlined, CloseOutlined, LineOutlined } from '@ant-design/icons';
+import { CloseOutlined } from '@ant-design/icons';
 import React from 'react';
 import isElectron from 'is-electron';
 
@@ -7,9 +7,9 @@ const ElectronButton: React.FC = () => {
   if (isElectron()) {
     return (
       <>
-        <Button type="link" id="electron_minimize" icon={<LineOutlined />} />
+        {/* <Button type="link" id="electron_minimize" icon={<LineOutlined />} /> */}
         {/* <Button type="link" id="electron_maximize" icon={<FullscreenExitOutlined />} /> */}
-        <Button type="link" id="electron_restore" icon={<ExpandAltOutlined />} />
+        {/* <Button type="link" id="electron_restore" icon={<ExpandAltOutlined />} /> */}
         <Button
           type="link"
           icon={<CloseOutlined />}
