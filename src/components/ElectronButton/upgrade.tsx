07bcbc0ba4fb5c3e661f7fc573e7dd7614ed31c3
nanli <linanenv@gmail.com>
2021-12-22 22:50:58 +0800

    fix: update button

--- src/components/ElectronButton/upgrade.tsx
+++ src/components/ElectronButton/upgrade.tsx
@@ -1,32 +1,40 @@
 import { Button, Modal } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import type { FC } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import isElectron from 'is-electron';
 
 const ElectronUpgrade: FC = () => {
+  const [updateButton, setUpdateButton] = useState(false);
   // equivalent to componentDidMount
   useEffect(() => {
     if (isElectron()) {
+      window.ipcRenderer.on('software-update', () => {
+        setUpdateButton(true);
+      });
     }
   }, []);
   if (isElectron()) {
-    return (
-      <Button
-        id="electron_upgrade"
-        onClick={() => {
-          Modal.confirm({
-            title: 'Are you sure to upgrade?',
-            icon: <ExclamationCircleOutlined />,
-            content: '',
-            onOk() {},
-            onCancel() {},
-          });
-        }}
-      >
-        Upgrade
-      </Button>
-    );
+    if (updateButton) {
+      return (
+        <Button
+          id="electron_upgrade"
+          onClick={() => {
+            Modal.confirm({
+              title: 'Are you sure to upgrade?',
+              icon: <ExclamationCircleOutlined />,
+              content: '',
+              onOk() {
+                window.ipcRenderer.send('software-upgrade');
+              },
+              onCancel() {},
+            });
+          }}
+        >
+          Upgrade
+        </Button>
+      );
+    }
   }
   return <></>;
 };
