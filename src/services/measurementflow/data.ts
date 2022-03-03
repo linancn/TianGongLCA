5f99359c1c00af7ba0572024e790a28a519d41c0
hyy1987 <779003812@qq.com>
2022-03-03 22:55:27 +0800

    fix: delete measurement

--- src/services/measurementflow/data.ts
+++
@@ -1,9 +0,0 @@
-export type MeasurementFlow = {
-  pkid: number;
-  id: string;
-  projectId: number;
-  flowBaseId: string;
-  measurementBaseId: string;
-  asRef: boolean;
-  conversionRef: number;
-};
