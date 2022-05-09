3fdc9126303b75c5ae73b58b0830f4377fa55b3c
GJing <jingguo92@gmail.com>
2022-05-09 13:50:01 +0800

    locale: jp updates

---
+++ src/locales/ja-JP/category.ts
@@ -0,0 +1,11 @@
+export default {
+  'category.index': '索引',
+  'category.dataName': 'データ名',
+  'category.categoryType': 'カテゴリー',
+  'category.lastChange': '最後の変更',
+  'category.release': 'リリース',
+  'category.description': '説明',
+  'category.delete': '選択したデータを削除しますか。',
+  'category.deletesuccess': '削除しました！',
+  'category.version': 'バージョン',
+};
