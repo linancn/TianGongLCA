8992f42bcf7e79bf1344c872cadcd2a9d66b89af
hyy1987 <779003812@qq.com>
2022-05-14 00:31:29 +0800

    fix: electron message

--- src/components/ElectronButton/message.tsx
+++ src/components/ElectronButton/message.tsx
@@ -1,45 +1,84 @@
-import { Modal, Space, Spin } from 'antd';
+import { Modal, Space, Spin, message, Button } from 'antd';
 import type { FC } from 'react';
 import { useState, useEffect } from 'react';
 import isElectron from 'is-electron';
+import { CloseCircleOutlined } from '@ant-design/icons';
 
 const ElectronMessage: FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [message, setMessage] = useState<JSX.Element>();
+  const [modelTitle, setModelTitle] = useState<JSX.Element>();
+  const [modelMessage, setModelMessage] = useState<JSX.Element>();
+  const [modelFooter, setModelFooter] = useState<JSX.Element | null>(null);
   let allMessage = <></>;
+  let openModel = false;
+
   useEffect(() => {
     if (isElectron()) {
-      setIsVisible(true);
+      window.ipcRenderer.send('code');
       window.ipcRenderer.on('data', (_e: any, msg: any) => {
         allMessage = (
           // eslint-disable-next-line react-hooks/exhaustive-deps
           <>
             {msg}
-            <br />${allMessage}
+            <br />
+            {allMessage}
           </>
         );
-        setMessage(allMessage);
+        setModelMessage(allMessage);
       });
-      window.ipcRenderer.on('close', () => {
-        setIsVisible(false);
+      window.ipcRenderer.on('close', (_e: any, code: any) => {
+        console.log(code);
+        if (code == -1) {
+          console.log(code);
+          setModelTitle(
+            <Space size={'middle'}>
+              <Spin />
+              Loading...
+            </Space>,
+          );
+          setIsVisible(true);
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          openModel = true;
+        } else if (code == 0) {
+          setIsVisible(false);
+          if (openModel) {
+            message.success('Loaded Successfully!');
+          }
+          openModel = false;
+        } else {
+          if (!openModel) {
+            setIsVisible(true);
+            window.ipcRenderer.send('err');
+          }
+          setModelTitle(
+            <Space size={'middle'}>
+              <CloseCircleOutlined />
+              Failed To Load!
+            </Space>,
+          );
+          setModelFooter(
+            <Button
+              onClick={() => {
+                window.close();
+              }}
+            >
+              Exit
+            </Button>,
+          );
+        }
       });
     }
   }, []);
   if (isElectron()) {
     return (
       <Modal
-        title={
-          <Space size={'middle'}>
-            <Spin />
-            Loading...
-          </Space>
-        }
+        title={modelTitle}
         closable={false}
-        footer={null}
+        footer={modelFooter}
         visible={isVisible}
         width={400}
       >
-        <div style={{ height: '200px', width: '350px', overflowY: 'auto' }}>{message}</div>
+        <div style={{ height: '200px', width: '350px', overflowY: 'auto' }}>{modelMessage}</div>
       </Modal>
     );
   }
